25c3545f615a2b7821f15c7310a8fd86c31f7622
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-07-25 08:35:42 -0600

    Instalacion de paquetes, configuracion del archivo appsettings.json, program.cs y  Scaffold-DbContext

---
+++ PracticaSeedCode/Models/PracticaSeedCodeContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PracticaSeedCode.Models;
+
+public partial class PracticaSeedCodeContext : DbContext
+{
+    public PracticaSeedCodeContext()
+    {
+    }
+
+    public PracticaSeedCodeContext(DbContextOptions<PracticaSeedCodeContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=Meli\\SQLEXPRESS;Initial Catalog=PracticaSeedCode;Integrated Security=True;Encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__roles__3214EC07E268DE30");
+
+            entity.ToTable("roles");
+
+            entity.HasIndex(e => e.Name, "UQ__roles__737584F609B5D90F").IsUnique();
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__users__3214EC074E925ED2");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Email, "UQ__users__A9D10534E523987F").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.RoleId).HasColumnName("role_id");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.Users)
+                .HasForeignKey(d => d.RoleId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__users__role_id__4D94879B");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
